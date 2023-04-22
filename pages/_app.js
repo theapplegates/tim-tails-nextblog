c120c5534cb51848edfc88b88d514078414836e9
Paul <paul@theapplegates.com>
2023-04-22 16:35:22 +0000

    ddd

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
- import '@/css/docsearch.css' // Uncomment if using algolia docsearch
- import '@docsearch/css' // Uncomment if using algolia docsearch
+import '@/css/docsearch.css'
+import '@docsearch/css'
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
